070e5db830c6db2c3f171796bceb8b26e5b8f758
xandinho <xandibr@yahoo.com.br>
2024-04-26 21:06:10 -0300

    Primeiro Teste Automatizado

---
+++ cypress/e2e/3-loja-EBAC/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade:Login', ()=>{
+
+    it('Deve fazer login com sucesso', ()=>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('cavalcanti.teste@teste.com')
+        cy.get('#password').type('botafogo2010')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcanti.teste')
+    })
+})
