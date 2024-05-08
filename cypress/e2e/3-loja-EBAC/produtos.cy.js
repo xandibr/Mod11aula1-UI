1576e3d28c493389519c6861bde5bbaa447027b1
xandinho <xandibr@yahoo.com.br>
2024-05-08 19:12:29 -0300

    ATUALIZAÇÃO COM PRODUTOS

---
+++ cypress/e2e/3-loja-EBAC/produtos.cy.js
@@ -0,0 +1,33 @@
+///<reference types="cypress"/>
+
+describe('FUNCIONALIDADE: PRODUTOS', () => {
+
+    beforeEach(() => {
+        
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        
+    });
+
+    it('DEVE SELECIONAR UM PRODUTO DA LISTA', () => {
+        cy.get('.product-block').first().click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+
+        
+    });
+
+    it('DEVE SELECIONAR UM PRODUTO DA LISTA', () => {
+        cy.get('.product-block').last().click()
+        cy.get('#tab-title-description > a').should('contain','Descrição')
+    });
+
+    it('DEVE SELECIONAR UM PRODUTO DA LISTA', () => {
+        cy.get('.product-block').eq(4).click()
+        cy.get('#tab-title-description > a').should('contain','Descrição')
+        
+    });
+
+    it('DEVE SELECIONAR UM PRODUTO DA LISTA', () => {
+        cy.get('.product-block').contains('Atlas Fitness Tank').click()
+        
+    });
+});
