7ee868874f6bc3176e8688aceea0334d94e579f5
xandinho <xandibr@yahoo.com.br>
2024-07-11 20:15:40 -0300

    11deJulho

--- cypress/e2e/3-loja-EBAC/login.cy.js
+++ cypress/e2e/3-loja-EBAC/login.cy.js
@@ -12,24 +12,24 @@ describe('Funcionalidade:Login', ()=>{
     });
 
     it('Deve fazer login com sucesso', ()=>{
-        cy.get('#username').type('cavalcanti.teste@teste.com')
+        cy.get('#username').type('cavalcante.teste@teste.com')
         cy.get('#password').type('Botafogo2010')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcanti.teste')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcante.teste')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usário inválido', () => {
-        cy.get('#username').type('cavalcanti@teste.com')
+        cy.get('#username').type('cavalcante@teste.com')
         cy.get('#password').type('botafogo2010')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido.')
     });
     
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.get('#username').type('cavalcanti.teste@teste.com')
+        cy.get('#username').type('cavalcante.teste@teste.com')
         cy.get('#password').type('botogo2010')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error > li').should('contain','A senha fornecida para o e-mail cavalcanti.teste@teste.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error > li').should('contain','A senha fornecida para o e-mail cavalcante.teste@teste.com está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error > li').should('exist')
     });
 
@@ -37,7 +37,7 @@ describe('Funcionalidade:Login', ()=>{
         cy.get('#username').type(perfil.usuário)
         cy.get('#password').type(perfil.senha)
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcanti.teste')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcante.teste')
         
     });
 
@@ -46,15 +46,15 @@ describe('Funcionalidade:Login', ()=>{
         cy.get('#username').type(dados.usuário)
         cy.get('#password').type(dados.senha, {log : false})
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcanti.teste')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcante.teste')
         
     })
         
     });
 
-    it.only('Deve fazer login com sucesso - Usando Comandos Customizados', ()=>{
-        cy.login('cavalcanti.teste@teste.com', 'Botafogo2010')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcanti.teste')
+    it('Deve fazer login com sucesso - Usando Comandos Customizados', ()=>{
+        cy.login('cavalcante.teste@teste.com', 'Botafogo2010')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, cavalcante.teste')
     })
 
 })
